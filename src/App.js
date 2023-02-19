4cc746c5a41ec6be35c4c6cbb855d34c82fffce7
ihsankarim <ihsanalkarim303@gmail.com>
2023-02-19 20:44:30 +0700

    Component and props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import "./App.css";
+import './App.css';
 
 function App() {
     return (
-    <div className="App">
-        <Job salary={90000} position="Senior SDE" company="Amazon"/>
-        <Job salary={12000} position="Junior SE" company="Google" />
-        <Job salary={10000} position="Project Manager" company="Netflix" />
-         </div>
-    );
+        <div className="App">
+            <Job salary={9000} position="Senior SDE" company="Amazon"/>
+            <Job salary={12000} position="Junior SDE" company="Google"/>
+            <Job salary={10000} position="Digital marketing" company="Twitter"/>
+        </div>
+    )
 }
 
-// Create Component 
+
 const Job = (props) => {
     return (
-    <div>
-        <h1>{props.salary}</h1>
-        <h1>{props.position}</h1>
-        <h1>{props.company}</h1>
-    </div>
-    );
+        <div>
+            <h1>{props.salary}</h1>
+            <h1>{props.position}</h1>
+            <h1>{props.company}</h1>
+        </div>
+    )
 }
 
 export default App;
